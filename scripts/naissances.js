064a56b8970bd8d195225d05e555e91c01aaf33a
ksaidou <kougsaid1@gmail.com>
2025-02-06 23:44:48 +0000

    profils

---
+++ scripts/naissances.js
@@ -0,0 +1,506 @@
+const DECLARATIONS = [
+  {
+    id: "67a4f3282357df01d4a20691",
+    registred: "2016-06-01T03:36:32 -00:00",
+    child: {
+      birthdate: "2023-07-\t\t11T12:12:18 -00:00",
+      gender: "male",
+      firstname: "Day",
+      lastname: "Jacobs",
+    },
+    firstParent: {
+      birthdate: "2020-03-\t\t24T05:06:56 -00:00",
+      gender: "male",
+      firstname: "Concetta",
+      lastname: "Edwards",
+      email: "concettaedwards@enthaze.com",
+      phone: "+1 (803) 457-3952",
+      address: "174 Jefferson Street, Rockhill, Hawaii, 3033",
+    },
+    secondParent: {
+      birthdate: "2018-08-\t\t28T03:33:21 -00:00",
+      gender: "female",
+      firstname: "Caitlin",
+      lastname: "Johnston",
+      email: "caitlinjohnston@enthaze.com",
+      phone: "+1 (987) 499-3712",
+      address: "606 Dakota Place, Kimmell, Virginia, 532",
+    },
+    compagny: {
+      name: "BITREX",
+      address: "540 Irving Avenue, Waterford, Delaware, 8933",
+    },
+    comment:
+      "officia cupidatat cillum dolore est Lorem dolore consequat magna velit sunt proident ullamco id consequat laborum ea esse aliquip dolor in mollit anim in minim labore proident voluptate adipisicing aute",
+  },
+  {
+    id: "67a4f32875e941844325a5ca",
+    registred: "2016-05-03T07:58:42 -00:00",
+    child: {
+      birthdate: "2018-03-\t\t31T11:33:04 -00:00",
+      gender: "female",
+      firstname: "Lara",
+      lastname: "Ewing",
+    },
+    firstParent: {
+      birthdate: "2015-07-\t\t25T10:37:19 -00:00",
+      gender: "female",
+      firstname: "Mcintosh",
+      lastname: "Schultz",
+      email: "mcintoshschultz@bitrex.com",
+      phone: "+1 (946) 571-2991",
+      address: "498 Hampton Place, Deputy, District Of Columbia, 8747",
+    },
+    secondParent: {
+      birthdate: "2023-04-\t\t12T12:35:33 -00:00",
+      gender: "male",
+      firstname: "Vicki",
+      lastname: "Woodard",
+      email: "vickiwoodard@bitrex.com",
+      phone: "+1 (843) 415-3828",
+      address: "306 Llama Court, Duryea, Texas, 3744",
+    },
+    compagny: {
+      name: "INFOTRIPS",
+      address: "858 Gem Street, Clarksburg, Alaska, 1645",
+    },
+    comment:
+      "ea incididunt cupidatat in irure ad enim do nostrud eu minim dolor irure sunt Lorem excepteur culpa reprehenderit aute tempor culpa qui excepteur commodo occaecat ipsum quis cillum ipsum occaecat",
+  },
+  {
+    id: "67a4f328bb98a39a1d578345",
+    registred: "2024-04-06T02:49:52 -00:00",
+    child: {
+      birthdate: "2022-07-\t\t25T09:21:30 -00:00",
+      gender: "female",
+      firstname: "Duffy",
+      lastname: "Blackburn",
+    },
+    firstParent: {
+      birthdate: "2021-12-\t\t11T03:32:53 -00:00",
+      gender: "male",
+      firstname: "Rodgers",
+      lastname: "Chaney",
+      email: "rodgerschaney@infotrips.com",
+      phone: "+1 (842) 595-2902",
+      address: "925 Herkimer Street, Barrelville, Kentucky, 6911",
+    },
+    secondParent: {
+      birthdate: "2023-05-\t\t15T05:56:34 -00:00",
+      gender: "male",
+      firstname: "Summer",
+      lastname: "Castillo",
+      email: "summercastillo@infotrips.com",
+      phone: "+1 (951) 534-2612",
+      address: "153 Randolph Street, Courtland, Guam, 2438",
+    },
+    compagny: {
+      name: "VICON",
+      address: "788 Ryder Avenue, Marbury, Michigan, 3427",
+    },
+    comment:
+      "ex consectetur ad reprehenderit reprehenderit esse ad deserunt fugiat proident sunt fugiat deserunt ad ullamco non incididunt pariatur velit commodo cupidatat veniam cillum velit consequat non fugiat ea magna irure",
+  },
+  {
+    id: "67a4f3282dd0b267710bb292",
+    registred: "2015-12-31T07:33:31 -00:00",
+    child: {
+      birthdate: "2017-07-\t\t09T10:42:07 -00:00",
+      gender: "female",
+      firstname: "Klein",
+      lastname: "Mcintyre",
+    },
+    firstParent: {
+      birthdate: "2017-06-\t\t30T12:09:29 -00:00",
+      gender: "male",
+      firstname: "Alfreda",
+      lastname: "Booth",
+      email: "alfredabooth@vicon.com",
+      phone: "+1 (926) 595-2467",
+      address: "139 Bay Avenue, Wikieup, Marshall Islands, 431",
+    },
+    secondParent: {
+      birthdate: "2019-01-\t\t21T01:43:47 -00:00",
+      gender: "female",
+      firstname: "Ernestine",
+      lastname: "Horne",
+      email: "ernestinehorne@vicon.com",
+      phone: "+1 (982) 469-2267",
+      address: "743 Willow Place, Odessa, Wyoming, 1744",
+    },
+    compagny: {
+      name: "SHEPARD",
+      address: "746 Dunne Court, Stonybrook, California, 6374",
+    },
+    comment:
+      "nulla aute dolor aliqua cillum laboris irure quis duis velit ex anim cillum tempor commodo nostrud et fugiat esse tempor laboris reprehenderit aliqua in dolore magna officia est deserunt velit",
+  },
+  {
+    id: "67a4f328a935da565a0faf0f",
+    registred: "2022-06-24T12:57:45 -00:00",
+    child: {
+      birthdate: "2019-03-\t\t10T04:15:28 -00:00",
+      gender: "female",
+      firstname: "Gardner",
+      lastname: "Armstrong",
+    },
+    firstParent: {
+      birthdate: "2018-02-\t\t20T11:10:19 -00:00",
+      gender: "male",
+      firstname: "Dorothea",
+      lastname: "Harrison",
+      email: "dorotheaharrison@shepard.com",
+      phone: "+1 (939) 485-2960",
+      address: "168 Cumberland Street, Greenbush, Indiana, 7292",
+    },
+    secondParent: {
+      birthdate: "2021-01-\t\t06T05:00:01 -00:00",
+      gender: "female",
+      firstname: "Buchanan",
+      lastname: "Harrington",
+      email: "buchananharrington@shepard.com",
+      phone: "+1 (947) 406-3294",
+      address: "883 Kenmore Court, Connerton, Maryland, 1822",
+    },
+    compagny: {
+      name: "MITROC",
+      address: "941 Hampton Avenue, Biehle, Kansas, 2795",
+    },
+    comment:
+      "nisi nostrud amet proident aliqua commodo nisi consectetur minim enim nisi magna eiusmod irure magna quis voluptate eiusmod aliquip dolore occaecat dolor amet fugiat cupidatat ea dolore nisi nostrud elit",
+  },
+  {
+    id: "67a4f32840c522c255e57969",
+    registred: "2016-10-16T12:44:57 -00:00",
+    child: {
+      birthdate: "2018-01-\t\t23T01:03:13 -00:00",
+      gender: "male",
+      firstname: "Felecia",
+      lastname: "Bell",
+    },
+    firstParent: {
+      birthdate: "2021-05-\t\t23T04:28:25 -00:00",
+      gender: "female",
+      firstname: "Tamera",
+      lastname: "Wiley",
+      email: "tamerawiley@mitroc.com",
+      phone: "+1 (824) 594-3600",
+      address: "498 Cortelyou Road, Wildwood, Utah, 6996",
+    },
+    secondParent: {
+      birthdate: "2015-02-\t\t26T12:43:05 -00:00",
+      gender: "female",
+      firstname: "Dee",
+      lastname: "Newman",
+      email: "deenewman@mitroc.com",
+      phone: "+1 (849) 577-3429",
+      address: "740 Allen Avenue, Cumberland, Puerto Rico, 1131",
+    },
+    compagny: {
+      name: "SYNTAC",
+      address: "207 Celeste Court, Utting, West Virginia, 619",
+    },
+    comment:
+      "esse eu consequat anim in laboris sunt aliqua nulla magna dolor culpa labore minim eu laborum sint dolore tempor ipsum deserunt pariatur Lorem laboris ullamco magna consectetur qui elit elit",
+  },
+  {
+    id: "67a4f3280586899bb97796ed",
+    registred: "2018-04-20T04:14:32 -00:00",
+    child: {
+      birthdate: "2014-01-\t\t29T10:51:59 -00:00",
+      gender: "female",
+      firstname: "Virgie",
+      lastname: "Bryant",
+    },
+    firstParent: {
+      birthdate: "2020-06-\t\t29T06:28:19 -00:00",
+      gender: "female",
+      firstname: "Vicky",
+      lastname: "Ellison",
+      email: "vickyellison@syntac.com",
+      phone: "+1 (903) 464-3074",
+      address: "485 Village Court, Ryderwood, Ohio, 3264",
+    },
+    secondParent: {
+      birthdate: "2018-04-\t\t15T09:59:56 -00:00",
+      gender: "female",
+      firstname: "Bridges",
+      lastname: "Stevenson",
+      email: "bridgesstevenson@syntac.com",
+      phone: "+1 (962) 424-2621",
+      address: "695 Oak Street, Harrison, Rhode Island, 8872",
+    },
+    compagny: {
+      name: "ZOARERE",
+      address: "440 Louisa Street, Centerville, Tennessee, 4807",
+    },
+    comment:
+      "Lorem minim tempor ipsum eiusmod anim consequat quis ex magna quis ut elit in incididunt do eu velit et tempor et eiusmod culpa laborum sit laborum labore cillum minim elit",
+  },
+  {
+    id: "67a4f328ebfc2a6cecda583e",
+    registred: "2023-12-07T10:44:03 -00:00",
+    child: {
+      birthdate: "2020-06-\t\t12T01:56:55 -00:00",
+      gender: "male",
+      firstname: "Leigh",
+      lastname: "Baldwin",
+    },
+    firstParent: {
+      birthdate: "2018-07-\t\t30T12:12:24 -00:00",
+      gender: "female",
+      firstname: "Mable",
+      lastname: "Molina",
+      email: "mablemolina@zoarere.com",
+      phone: "+1 (889) 481-2715",
+      address: "755 Grafton Street, Springville, Montana, 4272",
+    },
+    secondParent: {
+      birthdate: "2016-02-\t\t14T08:49:54 -00:00",
+      gender: "female",
+      firstname: "Blackburn",
+      lastname: "Barnett",
+      email: "blackburnbarnett@zoarere.com",
+      phone: "+1 (850) 453-3400",
+      address: "415 Madison Place, Nile, Illinois, 3024",
+    },
+    compagny: {
+      name: "ZILLAN",
+      address: "612 Garden Street, Ronco, Maine, 4798",
+    },
+    comment:
+      "eiusmod ex Lorem cillum cupidatat Lorem veniam amet ipsum deserunt ullamco tempor ex ea excepteur fugiat aute sint laboris sunt in qui nostrud velit incididunt est laborum exercitation aute pariatur",
+  },
+  {
+    id: "67a4f328f8850f3183b9d901",
+    registred: "2023-04-05T10:38:11 -00:00",
+    child: {
+      birthdate: "2014-11-\t\t03T09:47:20 -00:00",
+      gender: "male",
+      firstname: "Carrillo",
+      lastname: "Cruz",
+    },
+    firstParent: {
+      birthdate: "2020-07-\t\t21T01:14:36 -00:00",
+      gender: "male",
+      firstname: "Stefanie",
+      lastname: "Newton",
+      email: "stefanienewton@zillan.com",
+      phone: "+1 (953) 545-3592",
+      address: "233 Applegate Court, Winston, New Jersey, 1905",
+    },
+    secondParent: {
+      birthdate: "2021-01-\t\t21T12:10:27 -00:00",
+      gender: "female",
+      firstname: "Socorro",
+      lastname: "Boyer",
+      email: "socorroboyer@zillan.com",
+      phone: "+1 (807) 429-3823",
+      address: "146 Oceanic Avenue, Vandiver, Oklahoma, 6231",
+    },
+    compagny: {
+      name: "EZENTIA",
+      address: "427 Knight Court, Dargan, Nebraska, 9398",
+    },
+    comment:
+      "duis fugiat deserunt ipsum dolor labore culpa magna excepteur sunt et et ea consequat ea reprehenderit mollit adipisicing ex enim commodo tempor mollit eiusmod et do anim proident veniam aliqua",
+  },
+  {
+    id: "67a4f328341a68706761a3c0",
+    registred: "2024-11-24T07:45:52 -00:00",
+    child: {
+      birthdate: "2018-09-\t\t23T12:10:02 -00:00",
+      gender: "female",
+      firstname: "Bell",
+      lastname: "Hull",
+    },
+    firstParent: {
+      birthdate: "2023-05-\t\t09T08:47:24 -00:00",
+      gender: "male",
+      firstname: "Weeks",
+      lastname: "Taylor",
+      email: "weekstaylor@ezentia.com",
+      phone: "+1 (885) 556-2436",
+      address: "645 Seacoast Terrace, Condon, American Samoa, 1759",
+    },
+    secondParent: {
+      birthdate: "2019-10-\t\t18T11:28:32 -00:00",
+      gender: "male",
+      firstname: "Elaine",
+      lastname: "Hays",
+      email: "elainehays@ezentia.com",
+      phone: "+1 (985) 451-3439",
+      address: "321 Gold Street, Lutsen, Minnesota, 1012",
+    },
+    compagny: {
+      name: "ZOLAVO",
+      address: "519 Woodside Avenue, Crisman, Idaho, 9089",
+    },
+    comment:
+      "enim adipisicing aute irure quis excepteur laboris exercitation aute ad labore qui incididunt commodo velit sit id amet velit cillum dolor proident commodo eu cupidatat mollit et elit consectetur veniam",
+  },
+  {
+    id: "67a4f328078048fb73b9204f",
+    registred: "2023-05-12T05:09:33 -00:00",
+    child: {
+      birthdate: "2021-03-\t\t31T07:44:03 -00:00",
+      gender: "female",
+      firstname: "Colette",
+      lastname: "Albert",
+    },
+    firstParent: {
+      birthdate: "2020-11-\t\t16T07:32:54 -00:00",
+      gender: "female",
+      firstname: "Abby",
+      lastname: "Neal",
+      email: "abbyneal@zolavo.com",
+      phone: "+1 (958) 442-3292",
+      address: "905 Morton Street, Crucible, Arizona, 5061",
+    },
+    secondParent: {
+      birthdate: "2023-04-\t\t01T12:48:25 -00:00",
+      gender: "female",
+      firstname: "Lori",
+      lastname: "Rowe",
+      email: "lorirowe@zolavo.com",
+      phone: "+1 (883) 569-2661",
+      address:
+        "132 Roosevelt Court, Gilgo, Federated States Of Micronesia, 9854",
+    },
+    compagny: {
+      name: "QABOOS",
+      address: "404 Goodwin Place, Roulette, Virgin Islands, 5163",
+    },
+    comment:
+      "quis aute nisi proident ipsum velit laboris incididunt sint laborum et quis excepteur adipisicing ea culpa ullamco proident labore et ut pariatur tempor eiusmod laborum sit cillum quis veniam cillum",
+  },
+  {
+    id: "67a4f3280715e865440b6324",
+    registred: "2023-01-24T12:58:14 -00:00",
+    child: {
+      birthdate: "2016-04-\t\t30T04:51:39 -00:00",
+      gender: "female",
+      firstname: "Hunt",
+      lastname: "Yates",
+    },
+    firstParent: {
+      birthdate: "2021-05-\t\t24T06:59:32 -00:00",
+      gender: "male",
+      firstname: "Misty",
+      lastname: "Fowler",
+      email: "mistyfowler@qaboos.com",
+      phone: "+1 (893) 550-2151",
+      address: "216 Emerald Street, Sims, Arkansas, 1594",
+    },
+    secondParent: {
+      birthdate: "2023-11-\t\t19T10:31:26 -00:00",
+      gender: "female",
+      firstname: "Corine",
+      lastname: "Rasmussen",
+      email: "corinerasmussen@qaboos.com",
+      phone: "+1 (974) 508-2802",
+      address: "446 Roebling Street, Lindcove, New York, 9502",
+    },
+    compagny: {
+      name: "SOLGAN",
+      address: "420 Russell Street, Brazos, North Carolina, 4156",
+    },
+    comment:
+      "consectetur nostrud consequat quis consequat adipisicing sint ullamco aliqua labore minim cupidatat non ut dolore cupidatat laborum enim ex qui amet eu commodo qui aliqua Lorem aliqua ad do nisi",
+  },
+  {
+    id: "67a4f3282d7a17988cbb7e8a",
+    registred: "2015-11-29T04:07:04 -00:00",
+    child: {
+      birthdate: "2024-04-\t\t07T01:21:37 -00:00",
+      gender: "female",
+      firstname: "Roslyn",
+      lastname: "Vance",
+    },
+    firstParent: {
+      birthdate: "2024-02-\t\t23T09:40:15 -00:00",
+      gender: "female",
+      firstname: "Mcclain",
+      lastname: "Mays",
+      email: "mcclainmays@solgan.com",
+      phone: "+1 (904) 565-3623",
+      address: "749 Elton Street, Ironton, Washington, 5329",
+    },
+    secondParent: {
+      birthdate: "2024-07-\t\t09T11:53:36 -00:00",
+      gender: "male",
+      firstname: "Parsons",
+      lastname: "Scott",
+      email: "parsonsscott@solgan.com",
+      phone: "+1 (943) 506-3500",
+      address: "611 Poly Place, Strykersville, Wisconsin, 8353",
+    },
+    compagny: {
+      name: "SULTRAX",
+      address: "211 Anna Court, Baker, New Mexico, 4145",
+    },
+    comment:
+      "nostrud ut qui id elit aute esse laboris amet dolore sint aliqua minim do occaecat eu officia quis labore anim esse occaecat cupidatat excepteur et sunt cupidatat dolor quis nulla",
+  },
+  {
+    id: "67a4f328c6d8d84aae6d0b9e",
+    registred: "2020-12-10T02:45:06 -00:00",
+    child: {
+      birthdate: "2015-07-\t\t27T01:39:20 -00:00",
+      gender: "male",
+      firstname: "Deborah",
+      lastname: "Riley",
+    },
+    firstParent: {
+      birthdate: "2015-05-\t\t07T08:08:23 -00:00",
+      gender: "female",
+      firstname: "Leann",
+      lastname: "James",
+      email: "leannjames@sultrax.com",
+      phone: "+1 (847) 519-2722",
+      address: "473 Krier Place, Geyserville, Vermont, 4155",
+    },
+    secondParent: {
+      birthdate: "2021-07-\t\t29T11:19:07 -00:00",
+      gender: "female",
+      firstname: "Connie",
+      lastname: "Hebert",
+      email: "conniehebert@sultrax.com",
+      phone: "+1 (994) 468-2896",
+      address: "877 Commercial Street, Hartsville/Hartley, Palau, 8286",
+    },
+    compagny: {
+      name: "QNEKT",
+      address: "658 Otsego Street, Cucumber, Alabama, 9984",
+    },
+    comment:
+      "commodo deserunt aliqua labore ut duis pariatur ut enim ut cupidatat deserunt cillum nisi magna pariatur pariatur id culpa sit cupidatat voluptate cupidatat laborum irure aliqua excepteur culpa velit minim",
+  },
+];
+
+function displayNaissances() {
+  console.log("affichage des naissances");
+  console.log(DECLARATIONS);
+
+  const DECLARATIONS_AS_TABLE = DECLARATIONS.map((item) => {
+    const ligne = `
+              <tr>
+                  <th scope="row">${item.registred}</th>
+                  <td>${item.child.firstname}</td>
+                  <td>${item.child.lastname}</td>
+                  <td>
+                     <span>${item.firstParent.firstname}</span>  
+                     <span>${item.firstParent.lastname}</span>
+                  </td>
+                  <td>
+                     <span>${item.secondParent.lastname}</span>
+                     <span>${item.secondParent.lastname}</span>
+                  </td>
+                  <td>Actions</td>                    
+              </tr>
+    `;
+    return ligne;
+  });
+  document.getElementById("declaration-table-body").innerHTML =
+    DECLARATIONS_AS_TABLE.join("");
+}
