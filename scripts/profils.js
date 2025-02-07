f2d34f4543f5ce158de3dd84310382482d60b761
ksaidou <kougsaid1@gmail.com>
2025-02-07 15:35:58 +0000

    Affichage des profils

--- scripts/profils.js
+++ scripts/profils.js
@@ -1,6 +1,24 @@
+function handleItems(items) {
+  console.log(items);
+  const ITEMS = items.map(
+    (item) => `
+        <div class="card" style="width: 18rem;">
+          <img src="https://randomuser.me/api/portraits/men/${item.id}.jpg" class="card-img-top" alt="Pas d'image">
+          <div class="card-body">
+            <h5 class="card-title">${item.name} title</h5>
+            <p class="card-text"> ${item.email}</p>
+            <p class="card-text"> ${item.phone}</p>
+            <a href="/frontend-naissances/profils/${item.id}" class="btn btn-primary">Voir profil</a>
+          </div>
+        </div>    
+    `
+  );
+  document.getElementById("profils-body").innerHTML = ITEMS.join("");
+}
+
 function displayProfil() {
   const URL = "https://jsonplaceholder.typicode.com/users";
   fetch(URL)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => handleItems(data));
 }
