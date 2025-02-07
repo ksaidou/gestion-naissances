11cab0efe7d0d8d22d418f58481929ec69abc205
ksaidou <kougsaid1@gmail.com>
2025-02-07 15:56:09 +0000

    destructuration

--- scripts/profils.js
+++ scripts/profils.js
@@ -1,14 +1,14 @@
 function handleItems(items) {
   console.log(items);
   const ITEMS = items.map(
-    (item) => `
+    ({ id, name, email, phone }) => `
         <div class="card" style="width: 18rem;">
-          <img src="https://randomuser.me/api/portraits/men/${item.id}.jpg" class="card-img-top" alt="Pas d'image">
+          <img src="https://randomuser.me/api/portraits/men/${id}.jpg" class="card-img-top" alt="Pas d'image">
           <div class="card-body">
-            <h5 class="card-title">${item.name} title</h5>
-            <p class="card-text"> ${item.email}</p>
-            <p class="card-text"> ${item.phone}</p>
-            <a href="/frontend-naissances/profils/${item.id}" class="btn btn-primary">Voir profil</a>
+            <h5 class="card-title">${name} title</h5>
+            <p class="card-text"> ${email}</p>
+            <p class="card-text"> ${phone}</p>
+            <a href="/frontend-naissances/profils/${id}" class="btn btn-primary">Voir profil</a>
           </div>
         </div>    
     `
